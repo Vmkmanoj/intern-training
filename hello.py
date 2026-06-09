3f54a25a903d9cc95db10db58d2e7328bc6bb8dd
Vmkmanoj <vmkmanoj13@gmail.com>
2026-06-09 10:05:16 +0530

    day 2 task

---
+++ hello.py
@@ -0,0 +1,7 @@
+name = input("Enter the name :")
+age = int(input("Enter the age :"))
+city = input("Enter the City : ")
+is_intern = True
+
+
+print(f'you name {name} your age is {age} your from {city} your  a {is_intern }')
