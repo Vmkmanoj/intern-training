7e8feede1d397a3a0d031e76037c0a843960e8ce
Vmkmanoj <vmkmanoj13@gmail.com>
2026-06-15 10:55:25 +0530

    Bank Application added

--- week 2/day 5/bankApplication.py
+++ week 2/day 5/bankApplication.py
@@ -4,26 +4,36 @@ class Bank:
         self.amount = amount
 
     def withdraw(self,amount):
+
+        if self.amount < amount:
+            return {"insufficient balance"}
+
         self.amount -= amount
         return {f"Your Current Balance is {self.amount}"}
     
     def deposit(self,amount):
-        self.deposi+= amount
+
+        if amount <= 0:
+            return {"enter the Currect Amount"}
+        
+        self.amount+= amount
         return {f"Your Current Balance is {self.amount}"}
     
     def balance(self):
         return {f"Your Current Balance is {self.amount}"}
-    
+
+bank = Bank("Manojkumar",1000)
 while True:
-    bank = Bank("Manojkumar",1000)
     print("""1.Balance \n 2.Deposit \n 3.withdraw""")
     choose = int(input("Enter the choose"))
     if choose == 1:
         print(bank.balance())
     elif choose == 2:
-        balance = int(input("Enter the Amount :"))
-        print(bank.deposit())
+        amount = int(input("Enter the Amount :"))
+        print(bank.deposit(amount))
     elif choose == 3:
-        balance = int(input("Enter the Amount"))
-        print(bank.withdraw())
+        amount = int(input("Enter the Amount"))
+        print(bank.withdraw(amount))
+    else:
+        break
 
