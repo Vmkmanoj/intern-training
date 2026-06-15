2c06e0b904576b8e1c79525e985123acf27b6e2b
Vmkmanoj <vmkmanoj13@gmail.com>
2026-06-15 10:37:40 +0530

    contect change to Contact

--- week 1/day 5/contactbook.py
+++ week 1/day 5/contactbook.py
@@ -5,7 +5,7 @@ class ContactBook:
         self.data = []
 
     def addContact(self):
-        print("---------Contect add-----------")
+        print("---------Contact add-----------")
         name = input("Enter the Name : ")
         age = int(input("Enter the Age : "))
         phoneNumber = int(input("Enter the phoneNumber : "))
@@ -21,25 +21,25 @@ class ContactBook:
         self.data.append(contactbook)
 
     def contactList(self):
-        print("---------list Contect----------")
+        print("---------list Contact----------")
         for i in self.data:
             print(i,end = "\n")
 
     def findContact(self):
         
-        name = input("enter the fiding contect")
+        name = input("enter the fiding contact")
 
         for i in self.data:
             if i["name"] in name:
                 print(i)
                 return
-        print("No contect found")
+        print("No contact found")
            
 
 def main():
     obj = ContactBook()
     while True:
-        print(""" 1.add conntect \n 2.contact list \n 3.findContect """)
+        print(""" 1.add conntect \n 2.contact list \n 3.find Contact """)
         choose = int(input("Enter the Choose : "))
         if choose == 1:
             obj.addContact()
