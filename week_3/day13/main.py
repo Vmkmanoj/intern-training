2357abc94b58c264beaf0109c5e84a02c33f6bfe
Vmkmanoj <vmkmanoj13@gmail.com>
2026-06-24 17:41:18 +0530

    Add SQLAlchemy models and ORM CRUD

--- week_3/day13/main.py
+++ week_3/day13/main.py
@@ -3,12 +3,13 @@ from app.router import apiRouter
 
 from app.database.base import Base
 from app.database.database import engine
+from app.migrations.runner import run_migrations
 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
+run_migrations()
 app.include_router(apiRouter)
 
